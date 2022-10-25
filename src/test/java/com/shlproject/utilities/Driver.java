8aa14f49315cedfacdf51d426e25d05236e122ac
Abubakr Shoaliev <shoalievabubakr@gmail.com>
2022-10-25 15:22:25 +0500

    setting up the project utilities and files

---
+++ src/test/java/com/shlproject/utilities/Driver.java
@@ -0,0 +1,33 @@
+package com.shlproject.utilities;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
+public class Driver {
+    private static WebDriver driver;
+
+    public static WebDriver getDriver() {
+        if (driver == null) {
+            String browser = ConfigurationReader.getProperty("browser");
+
+            switch (browser) {
+                case "chrome" :
+                    WebDriverManager.chromedriver().setup();
+                    driver = new ChromeDriver();
+                case "edge" :
+                    WebDriverManager.edgedriver().setup();
+                    driver = new EdgeDriver();
+            }
+
+            driver.manage().window().maximize();
+        }
+
+        return driver;
+    }
+
+    public static void closeDriver() {
+        driver.close();
+    }
+}
